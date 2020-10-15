0fb928593fdf9a01de02262a6e7b10d37a9221df
Daniel Bodmer <danny.bodmer@gmail.com>
2020-10-15 15:47:31 +0100

    update section13

--- main/js/section13/section13_theDOM.js
+++ main/js/section13/section13_theDOM.js
@@ -26,5 +26,32 @@ const inputsArr = [...inputs]; // turns into proper array
 console.log(inputsArr);
 
 // getElementsByClassName - same as above but returns by class rather than by tag
+const listItems = document.getElementsByClassName('special');
+console.dir(listItems);
+// find specific elements in the class
+const ul = document.getElementsByTagName('ul')[0];
+const p = ul.getElementsByClassName('special');
+const li = document.getElementsByTagName('ul')[0].getElementsByTagName('li');
+console.dir(li);
 
-// querySelector & querySelectorAll
+// querySelector & querySelectorAll - nicer what of getting elements!
+// can replicate functionality of getElementById, getElementsByTagName, getElementsByClassName
+/*
+pass in CSS style selector
+document.querySelector('h1') - find first matching tag
+document.querySelector('#red') - find first matching id
+document.querySelector('.big') - find first matching class
+use .querySelectorAll to return all instances of matching element
+*/
+const h1 = document.querySelector('h1');
+h1.addEventListener('click', () => (h1.innerHTML = 'I used a querySelector!'));
+
+const peas = document.querySelector('section ul li.special');
+console.dir(peas);
+
+const password = document.querySelector('input[type=password]');
+console.dir(password);
+password.placeholder = 'Please type your password';
+
+const allListItems = document.querySelectorAll('li');
+console.dir(allListItems);
