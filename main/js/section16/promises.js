499c2a0e16db48e62ee76d59e940bbc3b495fdd9
Daniel Bodmer <danny.bodmer@gmail.com>
2020-10-21 18:23:48 +0100

    update promise.js

--- main/js/section16/promises.js
+++ main/js/section16/promises.js
@@ -1,1 +1,13 @@
 // a better way to handle nested callbacks!
+// promise - an object representing the eventual completion or failure of an asynchronous operation
+// a promise always has two params - usually called resolve and reject
+const willGetYouADog = new Promise((resolve, reject) => {
+  const rand = Math.random();
+  rand <= 0.5 ? resolve() : reject();
+})
+  // every promise has a .then() method than will run if promise is resolved
+  .then(() => console.log('Promise Resolved!!!'))
+  // .catch() is run if promise is rejected
+  .catch(() => console.log('Promise Rejected!!'));
+
+// return promises from functions
